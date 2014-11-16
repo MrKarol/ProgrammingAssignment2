763f9f2da4ee1494fcec4770c941326ce1c87423
MrKarol <karoldorniak@gmail.com>
2014-11-16 15:45:02 +0000

    some typos in commentaries corrected

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
 ## function makeCacheMAtrix stores in its environment a matrix + its inverse
 ## the first time we call it with a new matrix then an inverse is set to NULL
-## to calculate an inverse of a matrix stored in makeChaceMatrix we need to go via cacheSolve function
+## to calculate an inverse of a matrix stored in makeCacheMatrix we need to go via cacheSolve function
 ## the whole problem could be resolved differently
 ## However, we were asked to use two functions to learn about lexical scoping and free variables
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
-        ## set function - stores new matrix in the makeCacheMatrix environment and calculates its inverse
+        ## set function - stores new matrix in the makeCacheMatrix environment
         set <- function(y) {
             ## we use <<- because we assign a value to x in the parent environment (makeCacheMatrix environment)
             x <<- y         ## new matrix stored 
@@ -27,19 +27,19 @@ makeCacheMatrix <- function(x = matrix()) {
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-## cacheSolve function calculates (if matrix not in cache) and returns an inverse of a matrix stored in  makeCacheMatrix environment
-## if an inverse of matrix exists in makeCacheMatrix environment then it will be returned
-## else new inverse will be calculated, stored in makeCacheMatrix environment and returned
-## argument x is a list returned by makeCacheMatrix
+## cacheSolve function returns an inverse of a matrix stored in makeCacheMatrix environment
+## if an inverse of matrix exists in makeCacheMatrix environment (cache) then it will be returned
+## else an inverse will be calculated, stored in makeCacheMatrix environment (cached) and returned
+## argument x is a list returned by makeCacheMatrix function
 ## we need to call makeCacheMArix before calling cacheSolve
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x', if inverse was not calculated yet then NULL is returned
+        ## x$getinv() returns an inverse of a matrix stored in makeCacheMatrix environment, if an inverse was not calculated yet then NULL is returned
         inv <- x$getinv()
         if(!is.null(inv)) {
             message("getting cached matrix from makeCacheMatrix environment")
-            return(inv)
+            return(inv) ## inverse returned and function exits here
         }
-        ## the below code runs if we do not have an inverse in the makeCacheMatrix environment (inverse not-cached yet)
+        ## the below code runs only if we do not have an inverse in the makeCacheMatrix environment (inverse was not-cached yet)
         data <- x$get()     #getting matrix stored in the makeCacheMatrix environment
         inv <- solve(data)  #calculating matrix inverse
         x$setinv(inv)       #storing matrix inverse inside makeCacheMatrix environment (caching)
